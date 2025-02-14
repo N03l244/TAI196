ce4e0ed3c240901fd31aa99954877f86ab58275c
Noel Betanzos De La Cruz <122043746@upq.edu.mx>
2025-02-13 18:58:36 -0600

    Practica 3
    verbos http

--- FASTAPI/main.py
+++ FASTAPI/main.py
@@ -1,5 +1,5 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from typing import Optional
 
 app= FastAPI(
@@ -18,46 +18,34 @@ usuarios=[
 def main():
     return {'hola FastApi','Noel'}
 
-@app.get('/promedio',tags=['Mi Calificacion TAI'])
-def promedio():
-    return 10
-#endPoint Parametro obligatorio
-@app.get('/usuario/{id}',tags=['Parametro Obligatorio'])
-def consultaUsuario(id:int):
-    #conectamos BD
-    #hacemos consulta, retornamos resultset
-    return{"Se encontro el usuario":id} 
+#endPoint Consultar todos
+@app.get('/usuarios', tags=['Operaciones CRUD'])
+def ConsultarTodos():
+    return {"Usuarios Registrados. ": usuarios}
 
+#endPoint para agrregar usuarios
+@app.post('/usuario/',tags=['Operaciones CRUD'])
+def AgregarUsuario(usuarionuevo: dict ):
+    for usr in usuarios: 
+        if usr["id"] == usuarionuevo.get("id"): 
+            raise HTTPException(status_code=400, detail="El id ya esta registrado") 
+    usuarios.append(usuarionuevo) 
+    return usuarionuevo 
 
-#endPoint Parametro opcional
-@app.get('/usuario/',tags=['Parametro opcional'])
-def consultaUsuario2(id:Optional[int]= None):
-        if id is not None:
-            for usuario in usuarios:
-                if usuario["id"] == id:
-                    return{"mensaje":"Usuario encontrado", "usuario":usuario}
-            return{"mensaje":f"No encontre el ID: {id}"}
-        else:
-            return{"mensaje": "No se proporciono un Id"}
+#endPoint para PUT
+@app.put("/usuario/{id}", tags=["Operaciones CRUD"])
+def actualizar(id: int, usuario_update: dict):
+    for index, usr in enumerate(usuarios):
+        if usr["id"] == id:
+            usuarios[index].update(usuario_update)
+            return usuarios[index]
+    raise HTTPException(status_code=404, detail="ID no encontrado")
 
-#endpoint con varios parametro opcionales
-@app.get("/usuarios/", tags=["3 parámetros opcionales"])
-async def consulta_usuarios(
-    usuario_id: Optional[int] = None,
-    nombre: Optional[str] = None,
-    edad: Optional[int] = None
-):
-    resultados = []
-
-    for usuario in usuarios:
-        if (
-            (usuario_id is None or usuario["id"] == usuario_id) and
-            (nombre is None or usuario["nombre"].lower() == nombre.lower()) and
-            (edad is None or usuario["edad"] == edad)
-        ):
-            resultados.append(usuario)
-
-    if resultados:
-        return {"usuarios_encontrados": resultados}
-    else:
-        return {"mensaje": "No se encontraron usuarios que coincidan con los parámetros proporcionados."}
+#endPoint para Delete
+@app.delete("/usuario/{id}", tags=["Operaciones CRUD"])
+def eliminar(id: int):
+    for index, usr in enumerate(usuarios):
+        if usr["id"] == id:
+            usuarios.pop(index)
+            return {"Usuario eliminado":id}
+    raise HTTPException(status_code=404, detail="ID no encontrado")
